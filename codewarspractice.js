dc7c472edc9c1560691fe1ef0d76dbfe2ab97e86
Mario Petkov <101120844+mariby21@users.noreply.github.com>
2022-11-14 12:41:14 +0200

    Update codewarspractice.js

--- codewarspractice.js
+++ codewarspractice.js
@@ -232,3 +232,16 @@ var summation = function (num) {
   return sum;
 };
 
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return True if you're better, else False!
+
+function betterThanAverage(classPoints, yourPoints) {
+  let average = classPoints.reduce((a, b) => a + b, 0) / classPoints.length;
+  return average < yourPoints;
+  // Your code here
+}
+
